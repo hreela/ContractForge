7f98e5b16488209b403c5857213d1176827f363f
TheBlockCodes <hreela@gmail.com>
2025-07-06 23:31:26 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,18 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://your-netlify-url.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
