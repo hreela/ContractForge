8bc3c985fa44db925227217060bed7ca79fc1514
TheBlockCodes <hreela@gmail.com>
2025-07-07 18:07:41 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ const config: Config = {
   darkMode: "class", // Enables class-based dark mode
 
   content: [
-    './client/index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  './client/index.html',
+  './client/src/**/*.{js,ts,jsx,tsx}',
+],
 
   theme: {
     extend: {
