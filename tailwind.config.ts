a511db72ef340c79f5051313b096a9deb7759624
TheBlockCodes <hreela@gmail.com>
2025-07-07 10:26:09 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,10 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      //  background: "var(--background)",
+     //   foreground: "var(--foreground)",
         card: {
           DEFAULT: "var(--card)",
           foreground: "var(--card-foreground)",
