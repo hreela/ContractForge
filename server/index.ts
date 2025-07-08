2fbd99f0df91ade97cb2f5be13d5fadefe5a9637
TheBlockCodes <hreela@gmail.com>
2025-07-08 09:08:13 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://your-netlify-url.netlify.app');
+  res.header('Access-Control-Allow-Origin', 'https://contractforge.in');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
