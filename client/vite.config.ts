c64d0cc15d9481db8a6ba2b68597461acfe09b11
TheBlockCodes <hreela@gmail.com>
2025-07-08 08:17:26 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
